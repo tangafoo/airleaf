b4fdb347b1122c37264f83c8855161822e58999d
Tan Ga Foo <tangafoo@Tans-MacBook-Pro.local>
2017-08-09 14:46:56 +0800

    1) Added routes for listings and users 2) Made very simple homepage

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
+  resources :listings
+  resources :users do
+    resource :listing
+  end
+
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "listings#index"
 end
